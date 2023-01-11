80ca23de5a368ec5128daf250f08f6542fb0228b
Miguel Manzano García <mmanzano@gmail.com>
2023-01-11 21:04:41 +0100

    Add a dependency to the HealthCheck route

--- src/Controller/HealthCheck/HealthCheckGetController.php
+++ src/Controller/HealthCheck/HealthCheckGetController.php
@@ -1,16 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller\HealthCheck;
 
+use App\Shared\Infrastructure\RandomNumberGenerator;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 final class HealthCheckGetController
 {
-    public function __invoke(): Response
+    public function __construct(private readonly RandomNumberGenerator $generator)
+    {
+    }
+
+    public function __invoke(Request $request): Response
     {
         return new JsonResponse([
             'status' => 'ok',
+            'number' => $this->generator->generate(),
         ]);
     }
 }
